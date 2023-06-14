d174e9f99f2b5196756f8b035dfa95347071c0e2
Samuel Maiko <sammymaiko721@gmail.com>
2023-06-14 19:09:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    
+return "This Saturday, I want to "+activity+"!";
+}
+
+function mondayWork(work="go to the office"){
+return "This Monday, I will "+work+".";
+}
+
+function wrapAdjective(adj="*") {
+
+    const epic=function (types="special") {
+        return "You are "+adj+types+adj+"!";
+    };
+    return epic;
+}
+
